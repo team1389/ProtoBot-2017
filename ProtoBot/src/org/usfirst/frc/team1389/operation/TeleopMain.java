bb8fc10112d587e3fb8ae1217fe46b68685bdce0
Ari Mindell <amindell11@gmail.com>
2017-01-28 18:03:24 -0500

    gyro working

--- ProtoBot/src/org/usfirst/frc/team1389/operation/TeleopMain.java
+++ ProtoBot/src/org/usfirst/frc/team1389/operation/TeleopMain.java
@@ -5,17 +5,23 @@ import org.usfirst.frc.team1389.robot.controls.ControlBoard;
 import org.usfirst.frc.team1389.systems.GripperSystem;
 import org.usfirst.frc.team1389.watchers.DebugDash;
 
+import com.team1389.hardware.inputs.software.AngleIn;
+import com.team1389.hardware.value_types.Position;
 import com.team1389.system.Subsystem;
 import com.team1389.system.SystemManager;
 import com.team1389.system.drive.CheesyDriveSystem;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 public class TeleopMain {
 	SystemManager manager;
 	ControlBoard controls;
 	RobotSoftware robot;
+	ADXRS450_Gyro gyro;
 
 	public TeleopMain(RobotSoftware robot) {
 		this.robot = robot;
+		this.gyro = new ADXRS450_Gyro();
 	}
 
 	public void init() {
@@ -24,7 +30,8 @@ public class TeleopMain {
 		Subsystem gripper = setupGripper();
 		manager = new SystemManager(drive, gripper);
 		manager.init();
-		DebugDash.getInstance().watch(drive, gripper);
+		DebugDash.getInstance().watch(drive, gripper,
+				new AngleIn<Position>(Position.class, gyro::getAngle).getWatchable("gyro"));
 	}
 
 	public Subsystem setupGripper() {
